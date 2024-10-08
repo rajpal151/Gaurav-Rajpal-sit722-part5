f9af6004c0d90fa69f71bbf47c50f8e24decc9f6
rajpal151 <81980197+rajpal151@users.noreply.github.com>
2024-10-08 22:43:37 +1100

    Update container-registry.tf

--- scripts/container-registry.tf
+++ scripts/container-registry.tf
@@ -1,6 +1,6 @@
 resource "azurerm_container_registry" "example" {
-  name                = var.acr_name
+  name                = rajpal151
   resource_group_name = azurerm_resource_group.example.name
-  location            = var.location
+  location            = eastus
   sku                 = "Basic"  # You can change the SKU to Premium or Standard if needed
 }
